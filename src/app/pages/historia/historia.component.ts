9bee376d76c207548b8b0e34c221397d85f3e0d0
Juan Jose Moreno Barron <jose.morenobrn@uanl.edu.mx>
2023-12-04 13:10:14 -0600

    Agregamos Swiper dinamico

--- src/app/pages/historia/historia.component.ts
+++ src/app/pages/historia/historia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-historia',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./historia.component.scss']
 })
 export class HistoriaComponent {
+  
+  @ViewChild('mySwiper') mySwiperRef: ElementRef | undefined;
+  swiper!: Swiper
 
+
+  constructor( ) {}
+
+  slides = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  addNumber() {
+    const length = this.slides.length;
+    this.slides.push(length + 1);
+    this.rechargeSwiper()
+    
+  }
+
+  deleteNumber() {
+    this.slides.pop();
+    this.rechargeSwiper()
+  }
+
+  rechargeSwiper(){
+    this.swiper = this.mySwiperRef?.nativeElement.swiper;
+    this.swiper = new Swiper(this.mySwiperRef?.nativeElement)
+    console.log({swiper: this.swiper });
+  }
 }
